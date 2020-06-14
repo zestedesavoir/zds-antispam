65641260af6bf83cf245151cd7c4d932a682e3f7
Situphen <situphen@gmail.com>
2020-06-14 22:14:18 +0200

    ~ First commit

---
+++ get-data.py
@@ -0,0 +1,22 @@
+# This file needs to be runned on the server with `/opt/zds/wrapper shell`
+# With this file, we get all the data we need from the database
+
+from django.db.models import Q, F
+from zds.member.models import Profile
+import json
+
+# For each user, we save their username and if they are banned or not
+# We also keep their biography, signature and website
+raw_data = list(Profile.objects.values('biography', 'can_read', username=F('user__username')))
+
+# We remove users when their biography, signature and website are empty
+data = []
+for elem in raw_data:
+    if elem['biography'] != "":
+        data.append(elem)
+
+# We write the file so it can be accessed on our website at https://zestedesavoir.com/static/antispam-data.json
+with open('/opt/zds/webroot/static/antispam-data.json', 'w') as f:
+    json.dump(data, f)
+
+# Do not forget to remove the file with `sudo -u zds rm /opt/zds/webroot/static/antispam-data.json`!
---
+++ training.py
@@ -0,0 +1,91 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.dummy import DummyClassifier
+from sklearn.svm import LinearSVC
+from pprint import pprint
+import json
+
+# Data importation
+
+_bio = []
+_can_read = []
+
+with open('./data.json', 'r') as f:
+    _data = json.load(f)
+
+for _elem in _data:
+    if not _elem['biography']:
+        continue
+
+    _bio.append(_elem['biography'])
+    _can_read.append(1 if _elem['can_read'] else 0)
+
+_limit = int(round(len(_bio) * 0.8))
+
+bio_train = _bio[:_limit]
+can_read_train = _can_read[:_limit]
+
+bio_test = _bio[_limit:]
+can_read_test = _can_read[_limit:]
+
+# Transformation text->number (text preprocessing, tokenizing and filtering of stopwords)
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(bio_train)
+
+# Frequency calculation
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+# Classifier training
+
+#clf = MultinomialNB().fit(X_train_tfidf, can_read_train)
+#clf = BernoulliNB().fit(X_train_tfidf, can_read_train)
+
+#clf = SGDClassifier(loss='squared_hinge', penalty='l2', alpha=1e-3,
+#                    max_iter=5000, tol=None, n_jobs=-1)
+clf = LinearSVC(max_iter=5000, loss='hinge')
+#clf = DummyClassifier("most_frequent")
+
+clf.fit(X_train_tfidf, can_read_train)
+
+# Prediction of test data
+
+X_new_counts = count_vect.transform(bio_test)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+predicted = clf.predict(X_new_tfidf)
+
+average = 0
+confusions = {}
+for real, pred in zip(can_read_test, predicted):
+    if real==pred:
+        average += 1
+
+    elem = 'real {} => pred {}'.format(real, pred)
+
+    if not confusions.get(elem, False):
+        confusions[elem] = [0, '']
+
+    confusions[elem][0] += 1
+
+for elem in confusions:
+    confusions[elem][1] = '{} %'.format(round(confusions[elem][0]/len(can_read_test)*100))
+
+print('\n', round(average/len(can_read_test)*100,2), '%\n\n')
+
+pprint(confusions)
+
+if False:
+    while True:
+        inpt = input('Enter a sentence (q to quit): ')
+
+        if inpt == "q":
+            break
+
+        X_new_counts = count_vect.transform([inpt])
+        X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+        print(clf.predict(X_new_tfidf)[0], '\n')
