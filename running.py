b24670d43fde4a1a10e4251697e87a109feb9d4c
Situphen <situphen@gmail.com>
2020-06-17 18:42:13 +0200

    Retrieve biographies with the API

--- running.py
+++ running.py
@@ -1,4 +1,31 @@
 from training import count_vect, tfidf_transformer, clf
+import requests
+
+
+def antispam(biography):
+    X_new_counts = count_vect.transform([biography])
+    X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+    return clf.predict(X_new_tfidf)[0]
+
+page_size = 10
+headers = {'Accept': 'application/json'}
+response = requests.get('https://zestedesavoir.com/api/membres/?page_size={}'.format(page_size), headers=headers)
+response.raise_for_status()
+members = response.json()['results']
+
+for member in members:
+    response = requests.get('https://zestedesavoir.com/api/membres/{}/'.format(member['id']), headers=headers)
+    response.raise_for_status()
+        
+    biography = response.json()['biography']
+    if biography == '':
+        print('∅  %s has no biography' % member['username'])
+        continue
+
+    if antispam(biography) == '0':
+        print("✘  %s's biography looks like spam" % member['username'])
+    else:
+        print("✔️  %s's biography doesn't look like spam" % member['username'])
 
 if __name__ == '__main__':
     while True:
@@ -7,7 +34,4 @@ if __name__ == '__main__':
         if inpt == "q":
             break
 
-        X_new_counts = count_vect.transform([inpt])
-        X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-
-        print(clf.predict(X_new_tfidf)[0], '\n')
+        print(antispam(inpt), '\n')
